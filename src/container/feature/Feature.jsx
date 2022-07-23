18b2777ef4c719725bcbb6e49b7d67791e9d39a9
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-23 17:17:40 +0700

    Set up file

---
+++ src/container/feature/Feature.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Feature.scss";
+const Feature = () => {
+  return <div>Feature</div>;
+};
+
+export default Feature;
---
+++ src/container/possibility/Posibility.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Posibility.scss";
+const Posibility = () => {
+  return <div>Posibility</div>;
+};
+
+export default Posibility;
