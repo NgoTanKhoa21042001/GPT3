2ad7c6d9e4a0e8bc85017db3f3f43bd9408e1511
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-26 10:39:11 +0700

    Hoàn thiện phần Feature

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,7 +2,55 @@ import React from "react";
 import "./Feature.scss";
 
 const Feature = () => (
-  <div className="gpt3__features section__padding" id="features"></div>
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-container">
+      <div className="gpt3__features-heading">
+        <h1 className="gpt3__features-title gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-content">
+        <div className="gpt3__features-desc">
+          <h1 className="gpt3__features-h1 global-heading">
+            Improving end distrusts instantly{" "}
+          </h1>
+          <p className="gpt3__features-text gpt3__paragrap">
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+          </p>
+        </div>
+        <div className="gpt3__features-desc">
+          <h1 className="gpt3__features-h1 global-heading">
+            Become the tended active
+          </h1>
+          <p className="gpt3__features-text gpt3__paragrap">
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+          </p>
+        </div>
+        <div className="gpt3__features-desc">
+          <h1 className="gpt3__features-h1 global-heading">
+            Message or am nothing
+          </h1>
+          <p className="gpt3__features-text gpt3__paragrap">
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+          </p>
+        </div>
+        <div className="gpt3__features-desc">
+          <h1 className="gpt3__features-h1 global-heading">
+            Really boy law county
+          </h1>
+          <p className="gpt3__features-text gpt3__paragrap">
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Feature;
