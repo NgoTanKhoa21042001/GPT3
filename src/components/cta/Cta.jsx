63c87d241affb1a12ee17dc93955fe9b156df590
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-26 17:33:51 +0700

    Hoàn thiện phần CTA và phần Posibility

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Cta.scss";
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
